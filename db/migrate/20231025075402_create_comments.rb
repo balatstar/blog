547e2cf8fb6eab677430064f3ba2d992d28415d8
joyce <joyce.ann.cruz@gmail.com>
2023-10-25 16:45:47 +0800

    Add post_id reference to comments and re-migrate

--- db/migrate/20231025075402_create_comments.rb
+++ db/migrate/20231025075402_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[7.1]
     create_table :comments do |t|
       t.text :text
       t.references :user, foreign_key: { to_table: :users }
+      t.references :post, foreign_key: { to_table: :posts }
 
       t.timestamps
     end
