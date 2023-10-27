ba73467f77b9f405a9fc7fbbb94f04dc3c2f5a5c
joyce <joyce.ann.cruz@gmail.com>
2023-10-27 14:37:56 +0800

    Fix linter errors

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -19,13 +19,13 @@ RSpec.describe Post, type: :model do
     expect(post).not_to be_valid
   end
 
-  it "increments posts_counter after create" do
+  it 'increments posts_counter after create' do
     user = User.create(name: 'Roy Batty', posts_counter: 5)
     Post.create(title: 'Sample Title', author: user)
     expect(user.reload.posts_counter).to eq(6)
   end
 
-  it "decrements posts_counter after destroy" do
+  it 'decrements posts_counter after destroy' do
     user = User.create(name: 'Roy Batty', posts_counter: 5)
     post = Post.create(title: 'Sample Title', author: user)
     post.destroy
@@ -40,39 +40,39 @@ RSpec.describe Post, type: :model do
     expect(recent_comments).to eq(comments.last(5).reverse)
   end
 
-  it "is valid with a comments_counter >= 0" do
-    user = User.create(name: "Roy Batty", posts_counter: 5)
-    post = Post.new(title: "Sample Title", author: user, comments_counter: 2)
+  it 'is valid with a comments_counter >= 0' do
+    user = User.create(name: 'Roy Batty', posts_counter: 5)
+    post = Post.new(title: 'Sample Title', author: user, comments_counter: 2)
     expect(post).to be_valid
   end
 
-  it "is not valid with a non-integer comments_counter" do
-    user = User.create(name: "Roy Batty", posts_counter: 5)
-    post = Post.new(title: "Sample Title", author: user, comments_counter: 2.5)
+  it 'is not valid with a non-integer comments_counter' do
+    user = User.create(name: 'Roy Batty', posts_counter: 5)
+    post = Post.new(title: 'Sample Title', author: user, comments_counter: 2.5)
     expect(post).not_to be_valid
   end
 
-  it "is not valid with a negative comments_counter" do
-    user = User.create(name: "Roy Batty", posts_counter: 5)
-    post = Post.new(title: "Sample Title", author: user, comments_counter: -1)
+  it 'is not valid with a negative comments_counter' do
+    user = User.create(name: 'Roy Batty', posts_counter: 5)
+    post = Post.new(title: 'Sample Title', author: user, comments_counter: -1)
     expect(post).not_to be_valid
   end
 
-  it "is valid with a likes_counter >= 0" do
-    user = User.create(name: "Roy Batty", posts_counter: 5)
-    post = Post.new(title: "Sample Title", author: user, likes_counter: 2)
+  it 'is valid with a likes_counter >= 0' do
+    user = User.create(name: 'Roy Batty', posts_counter: 5)
+    post = Post.new(title: 'Sample Title', author: user, likes_counter: 2)
     expect(post).to be_valid
   end
 
-  it "is not valid with a non-integer likes_counter" do
-    user = User.create(name: "Roy Batty", posts_counter: 5)
-    post = Post.new(title: "Sample Title", author: user, likes_counter: 2.5)
+  it 'is not valid with a non-integer likes_counter' do
+    user = User.create(name: 'Roy Batty', posts_counter: 5)
+    post = Post.new(title: 'Sample Title', author: user, likes_counter: 2.5)
     expect(post).not_to be_valid
   end
 
-  it "is not valid with a negative likes_counter" do
-    user = User.create(name: "Roy Batty", posts_counter: 5)
-    post = Post.new(title: "Sample Title", author: user, likes_counter: -1)
+  it 'is not valid with a negative likes_counter' do
+    user = User.create(name: 'Roy Batty', posts_counter: 5)
+    post = Post.new(title: 'Sample Title', author: user, likes_counter: -1)
     expect(post).not_to be_valid
   end
 end
