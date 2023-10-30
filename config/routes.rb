5516faac13c3e08a9044ab3cbe097c6eb19f0e56
joyce <joyce.ann.cruz@gmail.com>
2023-10-31 02:34:17 +0800

    Fix linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'users#index'
   resources :users, only: [:show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: %i[index show]
   end
 end
