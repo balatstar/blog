7262a667da2906ce0df108af4d60c50f87725dab
joyce <joyce.ann.cruz@gmail.com>
2023-11-02 23:51:27 +0800

    Add untracked files

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+    @posts = Post.paginate(page: params[:page], per_page: 2)
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+  end
+end
---
+++ spec/models/like_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  it "increments the post's likes_counter after create" do
+    user = User.create(name: 'Roy Batty', posts_counter: 5)
+    post = Post.create(title: 'Sample Title', author: user)
+    Like.create(user:, post:)
+    expect(post.reload.likes_counter).to eq(1)
+  end
+
+  it "decrements the post's likes_counter after destroy" do
+    user = User.create(name: 'Roy Batty', posts_counter: 5)
+    post = Post.create(title: 'Sample Title', author: user)
+    like = Like.create(user:, post:)
+    like.destroy
+    expect(post.reload.likes_counter).to eq(0)
+  end
+end
