e03a7e0b367d5e858891a9f101a7fe89ab454b55
Errytagedesign <errytage@gmail.com>
2023-11-09 22:00:00 +0100

    Update Ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
-
+​
 class Ability
   include CanCan::Ability
-
+​
   def initialize(user)
     # Define abilities for the user here. For example:
-    #
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
+    can :read, :all
+​
+    if user.admin?
+      can :manage, :all
+    else
+      can :create, [Post, Comment]
+      can :manage, Post, author_id: user.id
+      can :manage, Comment, user_id: user.id
+    end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
