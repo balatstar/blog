5ee42ae997d406ad68feea92edc92111c315193c
joyce <joyce.ann.cruz@gmail.com>
2023-11-10 21:23:28 +0800

    Install rswag gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   devise_scope :user do
     delete '/logout', to: 'devise/sessions#destroy', as: :logout
   end
